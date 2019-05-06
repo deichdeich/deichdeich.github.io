2503ee183adf6e285a3d557aa1798a8e4fe89769
Alex Deich <xdeich@gmail.com>
2019-05-06 08:44:58 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ require(
 
     var nparts = [5000, 5000, 5000];
     var sizes = [1, 2, 3];
-    var masses = [.1, 10, 10];
+    var masses = [.1, 5, 5];
     var choice = Math.floor(Math.random() * nparts.length);
     var nparticles = nparts[choice]; 
     var size = sizes[choice]; 
